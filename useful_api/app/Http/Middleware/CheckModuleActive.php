e3fdf219a90d47607ed2559f0427c445ea57198a
Votre Nom <christnelle.ehahoun-agossou@epitech.eu>
2025-10-17 01:35:42 +0100

    [UPDATE] update of middleware file

--- useful_api/app/Http/Middleware/CheckModuleActive.php
+++ useful_api/app/Http/Middleware/CheckModuleActive.php
@@ -14,21 +14,21 @@ class CheckModuleActive
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
 
-    public function handle(Request $request, Closure $next, $moduleName): Response
+    public function handle(Request $request, Closure $next, $moduleName)
     {
         $user = $request->user();
 
         if (! $user) {
-            return response()->json(['error' => 'Unauthenticated'], 401);
+            return response()->json(['error' => 'User unauthenticated'], 401);
         }
-
-        $module = $user->modules()
+        //vérifier si le module est actif
+        $module_isActive = $user->modules()
             ->where('name', $moduleName)
             ->wherePivot('is_active', true)
             ->first();
 
-        if (! $module) {
-            return response()->json(['error' => "Module '$moduleName' non actif"], 403);
+        if (! $module_isActive) {
+            return response()->json(['error' => "Module inactive. Please activate this modulee to use it."], 403);
         }
 
         return $next($request);
