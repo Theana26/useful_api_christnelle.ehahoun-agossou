ed6e33277f44d02b2bebe89e6ec737fae9110568
Votre Nom <christnelle.ehahoun-agossou@epitech.eu>
2025-10-17 01:34:22 +0100

    [UPDATE]Update of activate and desactivate methods and add of index method

--- useful_api/app/Http/Controllers/ModulesController.php
+++ useful_api/app/Http/Controllers/ModulesController.php
@@ -8,18 +8,43 @@ use Illuminate\Http\Request;
 class ModulesController extends Controller
 
 {
+    public function index()
+    {
+        $modules = Modules::select('id', 'name', 'description')->get();
+
+        return response()->json($modules, 200);
+    }
 
     public function activate($id, Request $request)
     {
-        $module = Modules::findOrFail($id);
-        $request->user()->modules()->syncWithoutDetaching([$id => ['is_active' => true]]);
-        return response()->json(['message' => "Module {$module->name} activé"]);
+        //chercher le module
+        $module = Modules::find($id);
+
+        //vérifier si le module existe
+        if (!$module){
+            return response()->json(["message"=>"Module not found"], 404);
+        }
+        //permet d'associer le module à l'utilisateur
+        $request->user()->modules()->syncWithoutDetaching([
+            $module->id => ['is_active' => true]
+        ]);
+
+        return response()->json(['message' => "Module {$module->name} activated"]);
     }
 
     public function desactivate($id, Request $request)
     {
         $module = Modules::findOrFail($id);
-        $request->user()->modules()->syncWithoutDetaching([$id => ['is_active' => false]]);
-        return response()->json(['message' => "Module {$module->name} désactivé"]);
+
+        if (!$module){
+            return response()->json(["message"=> "Module not found"], 404);
+        }
+
+
+        $request->user()->modules()->syncWithoutDetaching([
+            $module->id => ['is_active' => false]
+        ]);
+        
+        return response()->json(['message' => "Module {$module->name} désactivated"]);
     }
 }
